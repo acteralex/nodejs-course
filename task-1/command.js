c3210ef5129bf6f578c470e49693991efaf4473b
Aliaksandr Kokurin <web.alex.designer@gmail.com>
2020-10-03 21:56:25 +0300

    add logic to check required properties action and shift

---
+++ task-1/command.js
@@ -0,0 +1,38 @@
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .storeOptionsAsProperties(false)
+  .passCommandToAction(false)
+  .name('caesar-cipher');
+// .exitOverride();
+
+program
+  .option('-a, --action <action>', 'an action encode/decode') // TODO: use requiredOption, but how to handle error?
+  .option('-s, --shift <shift>', 'a shift') // TODO: use requiredOption, but how to handle error?
+  .option('-i, --input <input>', 'an input file')
+  .option('-o, --output <output>', 'an output file');
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+checkOnErrors();
+
+function checkOnErrors() {
+  const requiredOptionKeys = ['action', 'shift'];
+  const optionKeysWithErrors = getCheckRequiredOption(...requiredOptionKeys);
+
+  if (optionKeysWithErrors.length > 0) {
+    const errorMessage = `You have not set the ${optionKeysWithErrors.join(
+      ', '
+    )} properties. Set needed properties and run it again.`;
+    process.stderr.write(`${errorMessage}\n`);
+  }
+}
+
+function getCheckRequiredOption(...requiredOptionKeys) {
+  return requiredOptionKeys
+    .map(o => (options[o] === undefined ? o : null))
+    .filter(o => o !== null);
+}
